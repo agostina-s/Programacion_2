100957a390e06028e4d02c224bb94f3e617a4c30
agostina-s <agosaucedo17@gmail.com>
2023-08-01 09:25:29 -0300

    Cracion de componentes, modulos, y path de inicio, nosotros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './modules/inicio/pages/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",component:InicioComponent
+  },
+  {
+    path:"",loadChildren:()=>import('./modules/inicio/inicio.module').then(m=>m.InicioModule)
+  },
+  {
+    path:"",loadChildren:()=>import('./modules/nosotros/nosotros.module').then(m=>m.NosotrosModule)
+  },
+  {
+    path:"",loadChildren:()=>import('./shared/shared/shared.module').then(m=>m.SharedModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './componentes/login/login.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { ProductosComponent } from './modules/productos/productos.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { ButtonComponent } from './shared/componentes/button/button.component';
+import { FooterComponent } from './shared/componentes/footer/footer.component';
+import { NosotrosComponent } from './modules/nosotros/nosotros.component';
+import { InicioComponent } from './modules/inicio/pages/inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    ProductosComponent,
+    RegistroComponent,
+    ButtonComponent,
+    FooterComponent,
+    NosotrosComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
